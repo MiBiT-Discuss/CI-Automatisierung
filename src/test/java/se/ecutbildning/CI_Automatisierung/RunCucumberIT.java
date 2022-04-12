b0aa7f2b4a4300151d1212c554b79ad4d16268a8
MiBiT-Discuss <mibit@protonmail.com>
2022-04-12 22:37:33 +0200

    Cucumber tests show up in junit report.

--- src/test/java/se/ecutbildning/CI_Automatisierung/RunCucumberIT.java
+++ src/test/java/se/ecutbildning/CI_Automatisierung/RunCucumberIT.java
@@ -1,6 +1,7 @@
 package se.ecutbildning.CI_Automatisierung;
 
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
@@ -11,6 +12,7 @@ import org.junit.platform.suite.api.Suite;
 @IncludeEngines("cucumber")
 @SelectClasspathResource("se/ecutbildning/CI_Automatisierung")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "junit")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "se.ecutbildning.CI_Automatisierung")
 
 public class RunCucumberIT {
 }
